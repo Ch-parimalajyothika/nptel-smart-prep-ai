c5c91c7eae1ae743caf3336e38c9064f1ba79d17
Parimala Jyothika Chopparapu <parimalajyothika@gmail.com>
2026-03-24 21:44:38 +0530

    fixed index.js final

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,34 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// ✅ ADD THESE
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-
-// IF using toast
-import { Toaster } from 'react-hot-toast';
-
-// IF using custom notification
-// import { NotificationProvider } from './Notifications';
-
+// Create root
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Render app
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-
-        {/* If using toast */}
-        <Toaster position="top-right" />
-
-        {/* If using custom notification, use this instead */}
-        {/* <NotificationProvider> */}
-
-        <App />
-
-        {/* </NotificationProvider> */}
-
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
